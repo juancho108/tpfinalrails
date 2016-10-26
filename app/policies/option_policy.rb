8911ef1c931bc01351c57fac0b52cde161a0add5
juancho108 <jjf108@gmail.com>
2016-10-26 19:57:35 -0300

    Opciones Arregladas

--- app/policies/option_policy.rb
+++ app/policies/option_policy.rb
@@ -1,32 +1,40 @@
 class OptionPolicy < ApplicationPolicy
   	
-  	def index?
-      user.admin?
-    end
+	def index?
+    user.admin?
+  end
 
-    def show?
-      user.admin?
-    end
+  def show?
+    user.admin?
+  end
 
-    def create?
-      user.admin? 
-    end
+  def create?
+    user.admin? 
+  end
 
-    def new?
-      user.admin? 
-    end
+  def new?
+    user.admin? 
+  end
 
-    def update?
-      user.admin? 
-    end
+  def update?
+    user.admin? 
+  end
 
-    def edit?
-      user.admin?
-    end
+  def edit?
+    user.admin?
+  end
 
-    def destroy?
-      user.admin? 
-    end
+  def destroy?
+    user.admin? 
+  end
+
+  def resetear_cuentas_ml?
+    user.admin?
+  end
+
+  def actualizar_valor_dolar?
+    user.admin?
+  end
 
   class Scope < Scope
     def resolve
--- db/schema.rb
+++ db/schema.rb
@@ -156,7 +156,7 @@ ActiveRecord::Schema.define(version: 20160808160709) do
     t.datetime "created_at",                                             null: false
     t.datetime "updated_at",                                             null: false
     t.integer  "role",                   default: 0
-    t.datetime "last_sign_out_at",       default: '2016-09-26 19:00:37'
+    t.datetime "last_sign_out_at",       default: '2016-10-26 22:38:14'
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
